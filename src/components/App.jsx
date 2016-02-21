8a0e48fd375bed5911adce1fc37a01892400a665
ewgenius <ewgeniux@gmail.com>
2016-02-21 18:40:37 +0300

    responsive layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,22 @@ import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import Theme from '../theme.js';
 import React from 'react';
 import LeftNav from 'material-ui/lib/left-nav';
-import MenuItem from 'material-ui/lib/menus/menu-item';
+import AppBar from 'material-ui/lib/app-bar';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
 import DirectionsBus from 'material-ui/lib/svg-icons/maps/directions-bus';
+import IconButton from 'material-ui/lib/icon-button';
+import NavigationMenu from 'material-ui/lib/svg-icons/navigation/menu';
 import {Map, Marker, Popup, TileLayer, ZoomControl, Polyline} from 'react-leaflet';
 import {Icon} from 'leaflet';
 import io from 'socket.io-client';
 import routes from '../sources/dataRoutes.js';
 
+const sidebarWidth = 256;
+const responsiveWidth = 480;
+function expanded() {
+  return document.body.offsetWidth > responsiveWidth;
+}
 const apiUrl = location.hostname === 'localhost'
   ? 'http://localhost:3000'
   : 'https://kirov-bus.herokuapp.com';
@@ -29,10 +38,14 @@ class App extends React.Component {
       position: [
         51.505, -0.09
       ],
-      navOpen: true,
       scheme: [],
       busstops: [],
-      buses: []
+      buses: [],
+      sidebarDocked: expanded(),
+      sidebarOpen: expanded(),
+      mainStyle: {
+        left: expanded() ? sidebarWidth : 0
+      }
     };
 
     this.socket = io(apiUrl);
@@ -43,6 +56,19 @@ class App extends React.Component {
     this.socket.on('route.update', update => {
       console.log(update);
     });
+
+    window.onresize = this.resize.bind(this);
+  }
+
+  resize() {
+    this.setState({
+      sidebarDocked: expanded(),
+      sidebarOpen: expanded(),
+      mainStyle: {
+        left: expanded() ? sidebarWidth : 0
+      }
+    });
+    this.refs['map'].getLeafletElement().invalidateSize(true);
   }
 
   getChildContext() {
@@ -57,10 +83,14 @@ class App extends React.Component {
     }
   }
 
-  toggleNav() {
+  toggleSidebar(open) {
     this.setState({
-      navOpen: !this.state.navOpen
+      sidebarOpen: open,
+      mainStyle: {
+        left: expanded() ? (open ? sidebarWidth : 0) : 0
+      }
     });
+    this.refs['map'].getLeafletElement().invalidateSize(true);
   }
 
   getRoute(route) {
@@ -83,31 +113,40 @@ class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <LeftNav open={this.state.navOpen}>
-          {routes.map((route, i) => <MenuItem primaryText={route.name} leftIcon={<DirectionsBus />} key={i} onClick={() => this.getRoute(route)}/>)}
+        <LeftNav
+          docked={this.state.sidebarDocked}
+          open={this.state.sidebarOpen}
+          onRequestChange={open => this.toggleSidebar(open)}
+          width={sidebarWidth}>
+          <AppBar />
+          <List className="routes-list">
+            {routes.map((route, i) => <ListItem primaryText={route.name} leftIcon={<DirectionsBus />} key={i} onClick={() => this.getRoute(route)}/>)}
+          </List>
         </LeftNav>
-        <Map className="map" style={{
-          left: 256,
-          backgroundColor: '#242426'
-        }} center={this.state.position} zoom={12} zoomControl={false}>
-          <TileLayer url='http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png' attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'/>
-          {
-            this.state.busstops.map((stop, i) => <Marker key={i} icon={stopIcon} position={[Number(stop.lat), Number(stop.lng)]}>
-                <Popup>
-                  <span>{stop.stop_name}</span>
-                </Popup>
-              </Marker>)
-          }
-          {
-            this.state.buses.map((bus, i) => <Marker key={i} position={[Number(bus.lat), Number(bus.lng)]}>
-                <Popup>
-                  <span>{bus.info}</span>
-                </Popup>
-              </Marker>)
-          }
-          <Polyline color={Theme.palette.accent1Color} positions={this.state.scheme} />
-          <ZoomControl position='bottomright'/>
-        </Map>
+
+        <div className="main" style={this.state.mainStyle}>
+          <AppBar
+            iconElementLeft={<IconButton onClick={() => this.toggleSidebar(!this.state.sidebarOpen)}><NavigationMenu/></IconButton>}/>
+          <Map ref="map" className="map" center={this.state.position} zoom={12} zoomControl={false}>
+            <TileLayer url='http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png' attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'/>
+            {
+              this.state.busstops.map((stop, i) => <Marker key={i} icon={stopIcon} position={[Number(stop.lat), Number(stop.lng)]}>
+                  <Popup>
+                    <span>{stop.stop_name}</span>
+                  </Popup>
+                </Marker>)
+            }
+            {
+              this.state.buses.map((bus, i) => <Marker key={i} position={[Number(bus.lat), Number(bus.lng)]}>
+                  <Popup>
+                    <span>{bus.info}</span>
+                  </Popup>
+                </Marker>)
+            }
+            <Polyline color={Theme.palette.accent1Color} positions={this.state.scheme} />
+            <ZoomControl position='bottomright'/>
+          </Map>
+        </div>
       </div>
     );
   }
