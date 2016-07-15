c609ce1ae61c3934af671c7a2a02391815c19f0e
ewgenius <ewgeniux@gmail.com>
2016-07-15 15:19:38 +0300

    icons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,20 +3,48 @@ import {Component, PropTypes} from 'react'
 import * as injectTapEventPlugin from 'react-tap-event-plugin'
 import {MuiThemeProvider, getMuiTheme, lightBaseTheme} from 'material-ui/styles'
 
+// components
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
+import {List, ListItem} from 'material-ui/List'
 
+//icons
+import DirectionsBus from 'material-ui/svg-icons/maps/directions-bus'
+
+// config
 require('../../../src/styles/main.scss')
 injectTapEventPlugin()
 
 const theme = getMuiTheme(lightBaseTheme)
 
 export class App extends Component<any, any> {
+  state = {
+    routes: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+    ]
+  }
+
   render() {
+    const {routes} = this.state
+
     return <MuiThemeProvider muiTheme={theme}>
-      <div className='app'>
-        <AppBar title='kirov bus'/>
-        <Drawer />
+      <div className='app view'>
+        <AppBar title='Выберите маршрут'/>
+        <Drawer open={false}/>
+
+        <div className='content'>
+          <List>
+            {
+              routes.map((route, i) => {
+
+                return <ListItem
+                  key={i}
+                  primaryText={`Маршрут ${route}`}
+                  leftIcon={<DirectionsBus />}/>
+              })
+            }
+          </List>
+        </div>
       </div>
     </MuiThemeProvider>
   }
