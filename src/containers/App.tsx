c19483c5c0a1f1c1c0355e2dde4a30cba9f8e33e
ewgenius <ewgeniux@gmail.com>
2016-07-17 23:56:04 +0300

    upd

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {Component, PropTypes} from 'react'
 import {Provider} from 'react-redux'
-import {Router, Route, hashHistory} from 'react-router'
+import {Router, Route, Redirect, hashHistory} from 'react-router'
 import * as injectTapEventPlugin from 'react-tap-event-plugin'
 import {MuiThemeProvider} from 'material-ui/styles'
 
@@ -16,12 +16,19 @@ injectTapEventPlugin()
 
 const store = configureStore()
 
+const Dummy = () => <div>dummy</div>
+
 export default class App extends Component<any, any> {
   render() {
     return <Provider store={store}>
       <MuiThemeProvider muiTheme={theme}>
         <Router history={hashHistory}>
           <Route path='/' component={Shell}>
+            <Redirect from='/' to='/routes'/>
+            <Route path='/routes' component={Dummy}/>
+            <Route path='/favorite' component={Dummy}/>
+            <Route path='/settings' component={Dummy}/>
+            <Route path='/about' component={Dummy}/>
           </Route>
         </Router>
       </MuiThemeProvider>
